03794af7c0ad24a033fe24b24239901d68801d2e
Ronique Ricketts <johnoyrick@gmail.com>
2018-06-23 08:05:25 -0500

    Add and setup react router

--- wpm/src/App.js
+++ wpm/src/App.js
@@ -1,21 +1,16 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'; 
+import Home from './components/Home';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+const App = () => (
+  <Router>
+      <div>
+        <switch>
+          <Route exact path="/" component={Home} />
+        </switch>
       </div>
-    );
-  }
-}
+  </Router> 
+)
 
 export default App;
