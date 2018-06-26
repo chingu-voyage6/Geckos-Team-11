10e5575e7b1138ef0ad45ec0dc8d4b4a860fa101
Denis Rawls <denrawls@gmail.com>
2018-06-26 11:13:16 -0400

    Corrected Homepage.js

--- wpm/src/Homepage.js
+++ wpm/src/Homepage.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './Hpage.css';
 
-class App extends Component {
+class Homepage extends Component {
   render() {
     return (
       <div className="App">
@@ -30,4 +30,4 @@ class App extends Component {
   }
 }
   
-export default App;
+export default Homepage;
