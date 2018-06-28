c4f60eae6db28dfeb33ed84f7a1523e40accc073
Ronique Ricketts <johnoyrick@gmail.com>
2018-06-28 17:29:32 -0500

    designed landing page

--- wpm/src/components/Landingpage.js
+++ wpm/src/components/Landingpage.js
@@ -7,18 +7,19 @@ import React, { Component } from 'react';
           <section id='login-section'>
             <h1>TYPE</h1>
             <div id='login-options'>
-              <p>GET IN WITH</p>
-              <button id='facebook-btn'>Facebook</button>
-              <button id='google-btn'>Google</button>
+              <p className='padding-ten'>GET IN WITH</p>
+              <button id='facebook-btn' className='homepage-btn'>Facebook</button>
+              <button id='google-btn' className='homepage-btn'>Google</button>
             </div>
             <div id='home-footer'>
-              <a>PRIVACY</a>
-              <a>TERMS</a>
+              <a href='/privacy'>PRIVACY</a>
+              <a href='/privacy'>TERMS</a>
             </div>
           </section>            
           <article id='home-article'>
             <h2 id='home-article-header'>Improve your Typing Skills with your friends</h2>
             <p id='home-article-para'>If you type adeptly with 10 fingers, you're typing faster than your mind is working.
+              <br/>
               <span>James A. Michener</span>
             </p>
           </article>
