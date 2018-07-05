bd70bea75cb40e777e3af98d05f49fe0e33b9bca
Denis Rawls <denrawls@gmail.com>
2018-07-05 17:34:47 -0400

    added nav to hompage and profile

--- wpm/src/components/Homepage.js
+++ wpm/src/components/Homepage.js
@@ -2,41 +2,14 @@ import React, { Component } from 'react';
 import '../App.css';
 import '../Navbar.css'
 
+import Nav from '../partials/Nav';
+
 class Homepage extends Component {
-
-	constructor(props){
-		super( props )
-		this.state = {show: false};
-
-		this.dropDown = this.dropDown.bind(this)
-
-	}
-
-	dropDown = () => {
-		const { show } = this.state;
-		this.setState( { show: !show } )
-	}
   render() {
     return (
       <div className="App">
 		 <body>
-		     <header>	
-      		<a className="linkNav" href="/">
-      		<div className="navLogo">
-      			<p>
-      				<span>T</span>
-      				YPE.
-      			</p>
-      		</div>
-      		</a>
-    	<div className="menu">
-    	<div className="menuPic"onClick={ this.dropDown }>
-    	<img alt="Profilepic" src="https://images.unsplash.com/photo-1498354136128-58f790194fa7?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8d66d0f68c7412e69612414926dbbe97&auto=format&fit=crop&w=1350&q=80"/>
-    	</div>
-    	<br />
-    { this.state.show && <Box /> }
-    	</div>
-    </header>
+     <Nav />
 			<div className="testContainer">
 				<div className="practiceTest">
 					<div className="topBar">
