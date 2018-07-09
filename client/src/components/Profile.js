e59f0910d305974e09fb26b4dcb9b7c3ededd37e
Denis Rawls <denrawls@gmail.com>
2018-07-09 11:47:31 -0400

    revised Css and added testbar

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 
+import Testbar from '../partials/Testbar';
 import Progressbar from '../partials/Progressbar';
 import typewriter from '../assets/typewriter.jpeg';
 import Nav from '../partials/Nav';
@@ -7,33 +8,60 @@ import Nav from '../partials/Nav';
   class Profile extends Component{
     render(){
       return(
+        <div className="profileContainer">
         <main id='profile-main'>
-          <div id='user'>
-            <img id='profile-pic' src={typewriter} width='200px' height='200px' alt='user' />
-            <h3>User Name</h3>
-            <p>Server Name</p>
-          </div>
-          <div id='user-score'>           
-            <h3>WPM</h3>
-            <div id='user-wpm-results'>
-              <div  className='user-result' id='wpm-avg'>
-                <h4>Avg</h4>
-                <p>00</p>
-                <span>wpm</span>
-              </div>
-              <div className='user-result' id='wpm-best'>
-                <h4>Best</h4>
-                <p>00</p>
-                <span>wpm</span>
-              </div>
+          <div className='profileUser_left'>
+            <img id='profile-pic' src={typewriter} alt='user' />
+            <div className="profileBadge">
+              <svg className='svg-icon' viewBox='0 0 20 20'></svg>
+              <p></p>
             </div>
-            <h3>RECENT</h3>  
-            <div id='recent-progress'>
-              <Progressbar />      
-            </div>                     
+            <div className='profile-infosmall'>
+            <h1>User Name</h1>
+            <p>Server Name</p>
+            </div>
           </div>
-          <Nav />
+            <div className='profileUser_right'>
+                <div id='user-Wpm'>
+                    <div className='profileUser_title'>
+                        <p>WPM</p>
+                       <div className='profileUser_underline'>
+                          <div className='profileUser_MULine'></div>
+                          <div className='profileUser_BMULine'></div>
+                       </div> 
+                    </div>           
+                    
+                    <div id='user-wpm-results'>
+                      <div  className='user-result' id='wpm-avg'>
+                        <p>Avg</p>
+                        <h1>00</h1>
+                        <span>wpm</span>
+                      </div>
+                      <div className='user-result' id='wpm-best'>
+                        <p>Best</p>
+                        <h1>00</h1>
+                        <span>wpm</span>
+                      </div>
+                    </div>
+
+                    <div className='user_Status'>
+                      <div className='profileUser_title'>
+                        <p>RECENT</p>  
+                          <div className='profileUser_underline'>
+                            <div className='profileUser_MULine'></div>
+                            <div className='profileUser_BMULine'></div>
+                          </div>
+                        </div>     
+                      <div className='user_MaxWpm'>
+                        <p>Max 200 WPM</p>
+                      </div> 
+                    <Testbar />
+                  </div>                  
+                </div>
+                  <Nav />
+            </div>
         </main>
+        </div>
       )
     }
   }
