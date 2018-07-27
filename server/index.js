5ded841b30088f9d150844ad33f93757d8fc5e01
Ronique Ricketts <johnoyrick@gmail.com>
2018-07-27 12:05:57 -0500

    added db and auth files

--- server/index.js
+++ server/index.js
@@ -6,18 +6,22 @@ const app = express();
 //This will be the built production version when the app is launched on HEROKU
 //app.use(express.static(path.join(__dirname, 'client/build')));
 
+//::::: SHOULD HANDLE ALL HOME REQUEST
 app.get('/', (req, res)=>{
     res.send('HELLO TYPER')
 });
 
+//::::: SHOULD HANDLE ALL BOARD REQUEST
 app.get('/board', (req, res)=>{
     res.send({'board':'Success!'})
 })
 
+//::::: SHOULD HANDLE ALL PROFILE REQUEST
 app.get('/profile', (req, res)=>{
     res.send({'profile':'Success!'})
 })
 
+//::::: SHOULD HANDLE ALL PRACTICE REQUEST
 app.get('practice', (req, res)=>{
     res.send({'practice':'Success!'})
 })
