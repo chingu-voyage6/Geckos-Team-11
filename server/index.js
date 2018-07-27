956b4d49bbdce7c8cd5c8a3ee060882adf73949f
Ronique Ricketts <johnoyrick@gmail.com>
2018-07-27 11:11:14 -0500

    moved routes to main index file

--- server/index.js
+++ server/index.js
@@ -6,6 +6,20 @@ const app = express();
 //This will be the built production version when the app is launched on HEROKU
 //app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.get('/', (req, res)=> res.send('HELLO TYPER'));
+app.get('/', (req, res)=>{
+    res.send('HELLO TYPER')
+});
+
+app.get('/board', (req, res)=>{
+    res.send({'board':'Success!'})
+})
+
+app.get('/profile', (req, res)=>{
+    res.send({'profile':'Success!'})
+})
+
+app.get('practice', (req, res)=>{
+    res.send({'practice':'Success!'})
+})
 
 app.listen(process.env.PORT || 3001, ()=> console.log(`>App listening on port ${process.env.PORT}`))
